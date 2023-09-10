bfb79da7a9689cc43a3710fc40af80b884b4a3ff
james-o <jaydhales@gmail.com>
2023-09-10 19:53:31 +0100

    Added verification Arguments

--- scripts/swap.ts
+++ scripts/swap.ts
@@ -1,5 +1,7 @@
 import { ethers } from "hardhat";
 
+const swap = "0x7c0Ea13D006ccBEc644a16a21995a73c472CE48d";
+
 const tokens = [
   "0xF60ED92Fe334a69E71FDB0dC3b7b6bBC0134AA97",
   "0x5AB319218641c626c43E687A2FAACad034C35E23",
@@ -7,13 +9,14 @@ const tokens = [
 
 async function main() {
   const ownerAddr = "0xd8500da651a2e472ad870cdf76b5756f9c113257";
-  const ownerSig = await ethers.getImpersonatedSigner(ownerAddr);
+  // const ownerSig = await ethers.getImpersonatedSigner(ownerAddr);
+  const [ownerSig] = await ethers.getSigners();
   const contractTokenA = await ethers.getContractAt("IERC20", tokens[0]);
   const contractTokenB = await ethers.getContractAt("IERC20", tokens[1]);
 
   console.log({
-    balanceA: await contractTokenA.balanceOf(ownerAddr),
-    balanceB: await contractTokenB.balanceOf(ownerAddr),
+    balanceA: ethers.formatEther(await contractTokenA.balanceOf(ownerAddr)),
+    balanceB: ethers.formatEther(await contractTokenB.balanceOf(ownerAddr)),
   });
 
   const swapContract = await ethers.deployContract("SwapProtocol", tokens);
@@ -38,7 +41,18 @@ async function main() {
   await liquidityTx.wait();
 
   console.log({
-    reserve: await swapContract.getReserve(),
+    reserve: (await swapContract.getReserve()).map((r) =>
+      ethers.formatEther(r)
+    ),
+  });
+
+  console.log({
+    "balanceA before swap": ethers.formatEther(
+      await contractTokenA.balanceOf(ownerAddr)
+    ),
+    "balanceB before swap": ethers.formatEther(
+      await contractTokenB.balanceOf(ownerAddr)
+    ),
   });
 
   const amtAtoSwap = ethers.parseEther("2");
@@ -46,12 +60,18 @@ async function main() {
   await swapContract.connect(ownerSig).swap(tokens[0], amtAtoSwap);
 
   console.log({
-    "balanceA after swap": await contractTokenA.balanceOf(ownerAddr),
-    "balanceB after swap": await contractTokenB.balanceOf(ownerAddr),
+    "balanceA after swap": ethers.formatEther(
+      await contractTokenA.balanceOf(ownerAddr)
+    ),
+    "balanceB after swap": ethers.formatEther(
+      await contractTokenB.balanceOf(ownerAddr)
+    ),
   });
 
   console.log({
-    reserveAfterSwap: await swapContract.getReserve(),
+    reserveAfterSwap: (await swapContract.getReserve()).map((r) =>
+      ethers.formatEther(r)
+    ),
   });
 }
 
